64a5a8558dee2cfec0ba85caae257a1c4f26926d
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-05-25 13:08:51 -0400

    isotope works on search results now

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,20 @@ $(document).ready(()=>{
     const imageBaseUrl = 'http://image.tmdb.org/t/p/';
     const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key=' +apiKey
 
+    var buttonsHtml = '';
+    for(let i=0; i<genreArray.length; i++){
+        buttonsHtml += `<button class="btn btn-success genre-button">${genreArray[i].name}</button>`
+    }
+    buttonsHtml += '<button class="btn btn-primary all-genre-button">All</button>'
+    $('#genre-buttons').html(buttonsHtml);
+
     $.getJSON(nowPlayingUrl,(nowPlayingData)=>{
         var nowPlayingHtml = getHTML(nowPlayingData);
         $('#movie-grid').html(nowPlayingHtml);
         $('.movie-poster').click(function(){
+            
+            // movieInfo();
+
             var thisMovieId = $(this).attr('movie-id');
             // console.log(thisMovieId);
             var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
@@ -21,6 +31,19 @@ $(document).ready(()=>{
                 $('#myModal').modal();
             })
         });
+        $grid = $('#movie-grid').isotope({
+            itemSelector: '.movie-poster'
+        });
+        $grid.imagesLoaded().progress( function() {
+            $grid.isotope('layout');
+        });
+        $('.genre-button').click(function(){
+            $grid.isotope({filter: '.'+this.innerText})
+        });
+        $('.all-genre-button').click(function(){
+            $grid.isotope({filter: '.movie-poster'})
+        });
+        $grid.isotope('destroy');
     });
 
 
@@ -33,7 +56,7 @@ $(document).ready(()=>{
         $.getJSON(searchUrl,(searchMovieData)=>{
             var searchMovieHtml = getHTML(searchMovieData);
             $('#movie-grid').html(searchMovieHtml);
-             $('.movie-poster').click(function(){
+            $('.movie-poster').click(function(){
                 var thisMovieId = $(this).attr('movie-id');
                 // console.log(thisMovieId);
                 var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
@@ -46,15 +69,46 @@ $(document).ready(()=>{
                     document.getElementById('modal-body-img').outerHTML = '<img src="' +imageBaseUrl + 'w780' + thisMovieData.backdrop_path+'" id="modal-body-img">';
                     $('#myModal').modal();
                 })
+            });
+            $grid = $('#movie-grid').isotope({
+                itemSelector: '.movie-poster'
             })
+            $grid.imagesLoaded().progress( function() {
+                $grid.isotope('layout');
+            });
+            $('.genre-button').click(function(){
+                $grid.isotope({filter: '.'+this.innerText})
+            });
+            $('.all-genre-button').click(function(){
+                $grid.isotope({filter: '.movie-poster'})
+            });
         });
     });
+
+    // function movieInfo(){
+    //     var thisMovieId = $(this).attr('movie-id');
+    //     var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
+    //     $.getJSON(thisMovieUrl,(thisMovieData)=>{
+    //         $('#myModalLabel').html(thisMovieData.title);
+    //         $('.tagline').html(thisMovieData.tagline);
+    //         $('.overview').html(thisMovieData.overview);
+    //         document.getElementById('modal-body-img').outerHTML = '<img src="' +imageBaseUrl + 'w780' + thisMovieData.backdrop_path+'" id="modal-body-img">';
+    //         $('#myModal').modal();
+    //     });
+    // };
 
 
     function getHTML(data){
         var newHtml = '';
         for(let i=0; i < data.results.length; i++){
-            newHtml += '<div class="col-sm-6 col-md-3 movie-poster" movie-id='+data.results[i].id+'>';
+            var thisMovieGenres = data.results[i].genre_ids;
+            var movieGenreClassList = " ";
+            for (let j=0; j<genreArray.length; j++){
+                if(thisMovieGenres.indexOf(genreArray[j].id) > -1){
+                    movieGenreClassList += genreArray[j].name + " ";
+                }
+            };
+            newHtml += '<div class="col-sm-6 col-md-3 movie-poster ' +movieGenreClassList+ '" movie-id='+data.results[i].id+'>';
                 if(data.results[i].poster_path != null){
                     var posterUrl = imageBaseUrl + 'w300' + data.results[i].poster_path;
                     newHtml += `<img src="${posterUrl}" class="posters">`;
